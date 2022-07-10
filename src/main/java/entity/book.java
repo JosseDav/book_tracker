158708ef9332a7fc7b9743f6c4facaabd2a31703
larvitar <jossedavila@gmail.com>
2022-07-09 22:18:17 -0400

    apparently I was in the wrong folder, removed everything and created a class

--- src/main/java/entity/book.java
+++ src/main/java/entity/book.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.g;
+package entity;
+
 /**
  *
  * @author josse
  */
-public class NewClass {
-    public static void main(String args[]){
-    }
-    }
+public class book {
+    
+}
